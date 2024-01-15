8f6a2d67fe755da0f763e9652709be6070d09952
b3nard <chumba101benard@gmail.com>
2024-01-15 21:04:04 +0300

    Freature: added websocket connection manager

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -1,1 +1,17 @@
 package manage
+
+import "github.com/gofiber/contrib/websocket"
+
+type Manager struct {
+	Connections map[string]*websocket.Conn
+}
+
+// Adds websocket connection to connection manager
+func (m *Manager) Add(username string, c *websocket.Conn) {
+	m.Connections[username] = c
+}
+
+// Removes websocket connection from the connection manager
+func (m *Manager) Remove(username string) {
+	delete(m.Connections, username)
+}
